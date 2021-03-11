6df70d16598dd6bb73e7b121cd04c69462191414
Snowpack Build <actions@github.com>
2021-03-11 12:04:18 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/index.css.proxy.js
+++ docs/dist/index.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = "body {\r\n  margin: 0;\r\n}";
+  const code = "body {\n  margin: 0;\n}";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
